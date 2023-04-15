b893718d905a5687cbc65905aecd49c31de02b99
Ahad1713 <khanazan761@gmail.com>
2023-04-15 21:12:51 +0530

    :iphone: Added responsiveness

--- src/components/landing/landing.js
+++ src/components/landing/landing.js
@@ -3,19 +3,21 @@ import gif from '../../assets/gif/blockchain1.gif';
 
 function App() {
   return (
-    <div className="flex bg-black h-screen">
-      <div className="w-1/2 flex items-center justify-center">
-        <img src={gif} alt="gif" />
+    <div className="flex flex-col lg:flex-row bg-black h-screen">
+      <div className="w-full lg:w-1/2 flex items-center justify-center">
+        <img src={gif} alt="gif" className="max-w-full h-auto lg:h-full" />
       </div>
-      <div className="w-1/2 flex flex-col justify-center items-center text-white">
-        <h1 className="text-Roboto text-6xl mb-4 text-right">
-          Lorem Ipsum
-        </h1>
-        <div className="text-right px-20">
-          <p className="text-lexand pt-20">
+      <div className="w-full lg:w-1/2 flex flex-col justify-center items-center text-white px-4 lg:px-12 xl:px-24">
+        <div className="text-center lg:text-right">
+          <h1 className="text-Roboto text-3xl lg:text-6xl mb-4">
+            Lorem Ipsum
+          </h1>
+          <p className="text-lexand mb-8 lg:mb-12 xl:mb-16 pt-16">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
           </p>
-          <p className="text-lexand pt-12">
+        </div>
+        <div>
+          <p className="text-lexand mb-8 lg:mb-12 xl:mb-16 text-right">
             Excepteur sint occaecat cupidatat non proident sunt in culpa qui officia deserunt mollit anim id est laborum, SRM KTR
           </p>
         </div>
