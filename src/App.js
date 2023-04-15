fc8c2c6942dac5e04ea3b9e2cb4423e711e95e71
Disha <dishay166@gmail.com>
2023-04-15 19:14:51 +0530

    :sparkles: footer added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import Navbar from "./components/layout/navbar";
+import Footer from "./components/layout/footer";
 
 function App() {
   return (
     <div>
       <Navbar/>
+      <Footer/>
+
     </div>  
   );
 }
---
+++ src/components/layout/footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+     <div className='absolute bottom-0 text-black bg-[#00CCB7] w-screen text-center py-1'>
+     <p>Made with 🤍 by IEEE SRM SB   (〜￣▽￣)〜</p>
+    </div>
+  )
+}
+
+export default Footer
