6d9aef07f9ab24a2e7956e8a6f2079b43fe5a46e
Ahad1713 <khanazan761@gmail.com>
2023-04-16 22:33:59 +0530

    :iphone: responsiveness

--- src/components/cards/cards.js
+++ src/components/cards/cards.js
@@ -3,12 +3,12 @@ import img from "../../assets/images/image 4.svg";
 const Cards = () => {
   return (
   <div className="bg-black">
-    <div className="text-Roboto text-2xl lg:text-6xl mb-4 text-white text-center pt-20">
+    <div className="text-Roboto text-2xl lg:text-4xl mb-4 text-white text-center pt-20">
         <h3>Whats in it for you?</h3>
     </div>
     <div>
-    <section className="bg-black h-screen flex justify-center items-center gap-x-16 text-white pt-[-10]">
-      <div className="w-[300px] h-[380px] bg-transparent cursor-pointer group perspective ">
+    <section className="bg-black min-h-screen flex flex-wrap lg:flex-nowrap justify-center items-center gap-x-16 text-white flex-grow">
+      <div className="w-[300px] h-[380px] bg-transparent cursor-pointer group perspective lg:py-0 py-8">
         <div className="relative preserve-3d group-hover:my-rotate-y-180 w-full h-full duration-1000">
           <div className="absolute backface-hidden border-2 w-full h-full">
             <img src={img} className="w-full h-full" />
@@ -25,7 +25,7 @@ const Cards = () => {
           </div>
         </div>
       </div>
-      <div className="w-[300px] h-[380px] bg-transparent cursor-pointer group perspective">
+      <div className="w-[300px] h-[380px] bg-transparent cursor-pointer group perspective lg:py-0 py-8">
         <div className="relative preserve-3d group-hover:my-rotate-y-180 w-full h-full duration-1000">
           <div className="absolute backface-hidden border-2 w-full h-full">
             <img src={img} className="w-full h-full" />
@@ -42,7 +42,7 @@ const Cards = () => {
           </div>
         </div>
       </div>
-      <div className="w-[300px] h-[380px] bg-transparent cursor-pointer group perspective">
+      <div className="w-[300px] h-[380px] bg-transparent cursor-pointer group perspective lg:py-0 py-8">
         <div className="relative preserve-3d group-hover:my-rotate-y-180 w-full h-full duration-1000">
           <div className="absolute backface-hidden border-2 w-full h-full">
             <img src={img} className="w-full h-full" />
