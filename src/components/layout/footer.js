dba4b2dcffe165325a62045e21340c70672bc19a
Disha <dishay166@gmail.com>
2023-04-16 22:18:13 +0530

    :art: res

--- src/components/layout/footer.js
+++ src/components/layout/footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-     <div className='absolute bottom-0 text-black bg-[#00CCB7] w-screen text-center py-1'>
+     <div className='absolute  bottom-0 text-black flex justify-center text-sm bg-[#00CCB7] w-screen text-center py-1 '>
      <p>Made with 🤍 by IEEE SRM SB   (〜￣▽￣)〜</p>
     </div>
   )
