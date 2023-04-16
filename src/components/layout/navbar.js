f4108cf1a376993843a6de5d1cce7c137ce159f8
Disha <dishay166@gmail.com>
2023-04-16 23:04:33 +0530

    :iphone: navbar responsiveness

--- src/components/layout/navbar.js
+++ src/components/layout/navbar.js
@@ -1,26 +1,62 @@
-import React from 'react'
+import { useState } from 'react';
 import { PopupButton } from '@typeform/embed-react'
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div className='fixed w-screen'>
-        <div className="bg-[#1E1E1E] flex justify-between px-10 py-2">
-      <div>
-        <img src="/images/logo.svg" alt='logo' className=''/>
-      </div>
-      <div className='flex text-[#FFFFFF] space-x-8 text-lexand'>
-        <ul className='flex items-center space-x-8 '>
-            <li className='hover:text-[#00CCB7]'>About us</li>
-            <li className='hover:text-[#00CCB7]'>About the event</li>
-            <li className='hover:text-[#00CCB7]'>Sponsors</li>
-        </ul>
-        <button className="bg-[#00CCB7] hover:bg-[#06f7e3] text-white font-bold py-2 px-2 rounded">
+      <div className='bg-[#1E1E1E] flex justify-between px-10 py-2'>
+        <div>
+          <img src='/images/logo.svg' alt='logo' />
+        </div>
+        <div className='hidden lg:flex text-[#FFFFFF] space-x-8 text-lexand'>
+          <ul className='flex items-center space-x-8'>
+            <li>About us</li>
+            <li>About the event</li>
+            <li>Sponsors</li>
+          </ul>
+          <button className='bg-[#FFFFFF] hover:bg-[#00CCB7] roundedbg-[#FFFFFF] text-[#1E1E1E] border rounded flex items-center px-2 py-0.5 text-sm'>
           <PopupButton id="q6rWVcPk" style={{ fontSize: 20 }} className="my-button">Register now</PopupButton>
-        </button>
+          </button>
+        </div>
+        <div className='lg:hidden flex'>
+          <button onClick={toggleMenu} className='focus:outline-none'>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              className='h-6 w-6 text-[#FFFFFF]'
+              fill='none'
+              viewBox='0 0 24 24'
+              stroke='currentColor'
+            >
+              <path
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                strokeWidth={2}
+                d='M4 6h16M4 12h16M4 18h16'
+              />
+            </svg>
+          </button>
+        </div>
       </div>
+      {isOpen && (
+        <div className='bg-[#1E1E1E] w-screen py-2 lg:hidden text-[#FFFFFF]'>
+          <ul className='flex flex-col items-center space-y-4'>
+            <li>About us</li>
+            <li>About the event</li>
+            <li>Sponsors</li>
+            <button className='bg-[#FFFFFF] text-[#1E1E1E] border rounded flex items-center px-2 py-0.5'>
+              Register now
+            </button>
+          </ul>
+        </div>
+      )}
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
