e622cadf06f5cbf9f2befafbf566f6f8d0838463
Disha <dishay166@gmail.com>
2023-04-17 19:25:36 +0530

    :iphone: res changed

--- src/components/teams/team.js
+++ src/components/teams/team.js
@@ -3,17 +3,16 @@ import team from '../../utils/people'
 
 const Team = () => {
   return (
-    <div className='text-lexand pt-10 text-white px-60 flex flex-wrap space-x-10'>
-      <div >
-        <h1 className="text-robotomono text-2xl pb-10 md:text-center lg:text-start">Meet The Team!</h1>
-        <p className='text-lexand lg:max-w-xl md:text-center lg:text-start '>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim dffan veniam, </p>
+    <div className='text-lexand pt-20 pb-20 text-white px-10 lg:px-20 xl:px-60' style={{ position: 'relative', zIndex: '1' }}>
+      <h1 className="text-robotomono text-2xl pb-10 text-center lg:text-left pl-12">Meet The Team!</h1>
+      <p className='text-lexand max-w-2xl text-justify pl-12'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim dffan veniam, </p>
+      <div className="list-none pt-20 justify-between grid md:grid-cols-2 lg:grid-cols-4">
+        {team.map((member, idx) => (
+          <a key={idx}>
+            <img src="/images/speaker.svg" className='w-[60%] mx-auto pt-10 md:w-[70%]' />
+          </a>
+        ))}
       </div>
-      <div className="list-none pt-20 justify-between pb-10 grid md:grid-cols-2 lg:grid-cols-4">
-            {team.map((member,idx) => (
-            //   <li key={idx}>{member.name}</li>
-            <a><img src="/images/speaker.svg" className='w-[60%] pt-10 md:w-[70%]'></img></a>
-            ))}
-     </div>
     </div>
   )
 }
