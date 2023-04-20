fb33f417e0b8dbec586b1bb0a1923cc304f78159
Ahad1713 <khanazan761@gmail.com>
2023-04-21 00:37:42 +0530

    :art: reorganised code

--- src/components/landing/landing.js
+++ src/components/landing/landing.js
@@ -21,6 +21,13 @@ function App() {
             <p className="text-lexand mb-8 lg:mb-12 xl:mb-16 text-justify px-6 md:px-10 lg:px-0">
             Join us for an engaging exploration of blockchain and discover the future of transactions, data storage, and online interactions on 25/04/2023 in TechPark 501 from 9am to 3pm IST. Don't miss out on this exciting opportunity to unlock the limitless potential of blockchain!
             </p>
+            <div className="flex justify-center lg:hidden">
+              <button className="bg-[#FFFFFF] hover:bg-[#00CCB7] text-[#1E1E1E] border rounded flex items-center px-4 py-2 text-sm">
+              <a href='https://docs.google.com/forms/d/e/1FAIpQLSesglhIb8WZXa_Cjl1HeMVuqWKXlC1Fhdl68UoNS1b7KIYmwQ/viewform?usp=sf_link'>
+                Register Now
+              </a>
+              </button>
+            </div>
           </div>
         </div>
       </div>
